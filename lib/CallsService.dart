2cfafe9aa1bb4c8f0e18c06d381a84024a5977be
mohamed tlili <foxweb585@gmail.com>
2023-06-13 21:48:36 +0100

    topcalls

--- lib/CallsService.dart
+++ lib/CallsService.dart
@@ -1,35 +1,33 @@
 import 'package:topcalls/Call.dart';
 import 'package:call_log/call_log.dart';
-class CallsService {
 
-  Future<List<Contact>>? fetch_top_cotnact()async{
+class CallsService {
+  Future<List<Contact>>? fetch_top_cotnact() async {
     try {
-      
       Iterable<CallLogEntry> entries = await CallLog.get();
       entries = await CallLog.query();
-      Map<String,Contact> callsmap =  {};
+      Map<String, Contact> callsmap = {};
       List<Contact> listcontact = [];
-      entries.forEach((element) { 
-        Contact? c0 =   callsmap[element.number ?? ""] ;
-        DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(element.timestamp ?? 0);
-        if(c0 != null)
-        {if(dateTime.isBefore(c0.lastcall)){
-           dateTime = c0.lastcall;
+      entries.forEach((element) {
+        Contact? c0 = callsmap[element.number ?? ""];
+        DateTime dateTime =
+            DateTime.fromMillisecondsSinceEpoch(element.timestamp ?? 0);
+        if (c0 != null) {
+          if (dateTime.isBefore(c0.lastcall)) {
+            dateTime = c0.lastcall;
+          }
         }
-        }
-       callsmap[element.number ?? ""] = Contact(element.number ?? "",dateTime ,(c0?.totalduration  ??0) + (element.duration ?? 0)); 
-       
+        callsmap[element.number ?? ""] = Contact(element.number ?? "", dateTime,
+            (c0?.totalduration ?? 0) + (element.duration ?? 0));
       });
-       callsmap.forEach((key, value) {
+      callsmap.forEach((key, value) {
         listcontact.add(value);
-       });
-       listcontact.sort((a, b) => b.totalduration.compareTo(a.totalduration));
-       
-       return listcontact;
+      });
+      listcontact.sort((a, b) => b.totalduration.compareTo(a.totalduration));
+      return listcontact;
     } catch (e) {
       print(e);
     }
     return [];
   }
-
 }
--- lib/Home.dart
+++ lib/Home.dart
@@ -13,56 +13,102 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  bool checked = false;
   CallsService callsService = CallsService();
   Widget build(BuildContext context) {
-
     return RefreshIndicator(
-      onRefresh: () async{
-        setState(() {
-        });
+      onRefresh: () async {
+        setState(() {});
       },
       child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 176, 172, 163),
+          actions: [
+            Row(
+              children: [
+                Text("Emergency numbers :"),
+                Checkbox(
+                    fillColor:
+                        MaterialStatePropertyAll(Color.fromRGBO(0, 0, 0, 1)),
+                    value: checked,
+                    onChanged: (box) {
+                      checked = box!;
+                      setState(() {});
+                    })
+              ],
+            ),
+          ],
+        ),
         body: FutureBuilder(
           future: callsService.fetch_top_cotnact(),
-          
-          builder: (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) { 
-                int totald ;
-                return ListView.builder(
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (BuildContext context, int index) { 
-                   totald = (snapshot.data?.elementAt(index).totalduration ?? 0);
-                    return Container(
-                      
-                      child: Card(
-                        
-                        color: (index == 0)? Colors.amber:Color.fromARGB(255, 226, 216, 216),
-                        child: Row(
-                          children: [
-                            Container(
-                              width: 25,
-                              child: (index == 0)?Icon(Icons.favorite):Text("  ${index - 1 }"),
-                            ),
-                            SizedBox(width: 10,),
-                            Container(
+          builder:
+              (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
+            if (snapshot.data?.isNotEmpty ?? false) {
+              int totald;
+              List<Contact> data = snapshot.data!
+                      .where((element) =>
+                          (checked ? (element.contact.length <= 3) : true))
+                      .toList() ??
+                  [];
+              return ListView.builder(
+                itemCount: data?.length,
+                itemBuilder: (BuildContext context, int index) {
+                  totald = (data?.elementAt(index).totalduration ?? 0);
+                  return Container(
+                    child: Card(
+                      color: (index == 0)
+                          ? Colors.amber
+                          : Color.fromARGB(255, 226, 216, 216),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 25,
+                            child: (index == 0)
+                                ? Icon(Icons.favorite)
+                                : Text("  ${index - 1}"),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Container(
                               width: 120,
-                              child: Text("N: "+(snapshot.data?.elementAt(index).contact ?? ""),softWrap: true,)),
-                              Container(
-                                width: 90,
-                                child: Text(" total duration : "+((totald < 10000)?totald.toString():(totald/1000).toInt().toString() + "k"),softWrap: true,),
-                              ),
-                              Container(
-                                width: 100,
-                                child: Text(" last call : " + (snapshot.data?.elementAt(index).lastcall).toString(),softWrap: true,),
-                              )
-                          ],
-                        ),
+                              child: Text(
+                                "N: " + (data?.elementAt(index).contact ?? ""),
+                                softWrap: true,
+                              )),
+                          Container(
+                            width: 90,
+                            child: Text(
+                              " total duration : " +
+                                  ((totald < 10000)
+                                      ? totald.toString()
+                                      : (totald / 1000).toInt().toString() +
+                                          "k"),
+                              softWrap: true,
+                            ),
+                          ),
+                          Container(
+                            width: 100,
+                            child: Text(
+                              " last call : " +
+                                  (data?.elementAt(index).lastcall).toString(),
+                              softWrap: true,
+                            ),
+                          )
+                        ],
                       ),
-                    );
-                   },
-                  
-                );
-           },
-          
+                    ),
+                  );
+                },
+              );
+            } else {
+              return RefreshIndicator(
+                  onRefresh: () async {
+                    setState(() {});
+                  },
+                  child: CircularProgressIndicator());
+            }
+          },
         ),
       ),
     );
